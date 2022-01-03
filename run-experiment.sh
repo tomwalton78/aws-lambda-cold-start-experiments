7c8fc49a1fd5518cfbef05fd0b42b0b5bbec7ebf
Tom Walton <tomwalton78@googlemail.com>
2022-01-03 14:45:01 +0000

    Update test run script
    Speeds up test by only requiring single deploy

--- run-experiment.sh
+++ run-experiment.sh
@@ -8,10 +8,12 @@ do
     esac
 done
 
+make deploy  # need to deploy each time to ensure each invocation is a cold start
+
 # On my machine/network it takes approx 2 mins per repeat
 for i in $(seq 1 $repeats);
 do
   echo "Starting test run ${i}"
-  make deploy  # need to deploy each time to ensure each invocation is a cold start
-  aws lambda invoke --function-name ColdStartJavaFunction /dev/null
+  # Run in background so all invocations occur at once, and all will be a cold start
+  aws lambda invoke --function-name ColdStartJavaFunction --qualifier testPC /dev/null &
 done
